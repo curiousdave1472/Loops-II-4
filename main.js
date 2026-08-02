1422063c0385d72669c005835f65cc937dc38e74
curiousdave1472 <davidshanlay@gmail.com>
2026-08-02 21:23:55 +0000

    Incorrect

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// Write your code below
+//Assign the function
+function cumulativeSum(int){
+    //Start sum from 0
+ sum = 0;
+    for(let sum = 0; sum<int; sum++){
+        sum = sum +1;
+    }
+    return sum;
+}
